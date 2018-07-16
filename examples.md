d16613269bdcac9b9ee9f821e85d5369cbdd9dd6
Roman Openyshev <openyshev@yandex-team.ru>
2018-07-16 10:46:27 +0300

    first commit

---
+++ examples.md
@@ -0,0 +1,133 @@
+> '2' == 2
+
+> '2' === 2
+
+> [2] == 2
+
+> [] + []
+
+> [] == []
+
+> typeof []
+
+> [] == ![]
+
+> +[] == +![]
+
+> 0 > null
+
+> 0 >= null
+
+> 0 == null
+
+> 0 <= null
+
+> 0 < null
+
+> typeof null
+
+> null instanceof Object
+
+> {}.toString.call([])
+
+> function FooBar() {}
+
+> {}.toString.call(new FooBar)
+
+> (new FooBar).constructor == Foo
+
+> [] instanceof Array
+
+> Array.isArray([])
+
+> 999999999999999
+
+> 9999999999999999
+
+> -9999999999999999
+
+> 0.1 + 0.2 == 0.3
+
+> 0.1 + 0.2
+
+> 3 > 2 > 1
+
+> 3 > 2 >= 1
+
+> '2' + 1
+
+> '2' - 1
+
+> '2' - -1
+
+> 1/0
+
+> typeof Infinity
+
+> 0 === -0
+
+> 1/0
+
+> 1/-0
+
+> Infinity == -Infinity
+
+> Number.MAX_VALUE
+
+> 0 - 1
+
+> Number.MIN_VALUE < 0
+
+> Number.MIN_VALUE
+
+> Math.max(1,2,3)
+
+> Math.min(1,2,3)
+
+> Math.max() > Math.min()
+
+> Math.max()
+
+> Math.min()
+
+> 'wtf' instanceof String
+
+> typeof 'wtf'
+
+> typeof String('wtf')
+
+> String('wtf') === 'wtf'
+
+> String('wtf') === new String('wtf')
+
+> String('wtf') == new String('wtf')]
+
+> new String('wtf')
+
+> new String('wtf').toString()
+
+> 'wft' + 1
+
+> 'wft' - 1
+
+> typeof NaN
+
+> typeof 1138
+
+> 1138 == 1138
+
+> NaN == NaN
+
+> isNaN('wft' - 1)
+
+> true + false
+
+> true + true == true
+
+> new Function('console.error("WTF")')()
+
+> Array instanceof Array
+
+> new Array instanceof Array
+
+> Math instanceof Math
---
+++ index.js
@@ -0,0 +1,195 @@
+const MODIFICATORS = [
+    'none',
+    '+VAR',
+    '-VAR',
+    '!VAR',
+    '+!VAR',
+    '-!!VAR',
+    '+!!VAR'
+];
+
+const VALUE_TYPES = [
+    'number',
+    'number_as_string',
+    'string_like_number',
+    'array_with_number',
+    'empty_object',
+    'string',
+    'empty_string',
+    'empty_array',
+    'nil',
+    'nan'
+];
+
+const STRING_AS_NUMBER_TYPES = [
+    'space_before',
+    'space_before_end_char',
+    'end_char',
+    'start_char',
+    'start_char_space_after',
+    'middle_char',
+    'space_before_middle_char',
+    'space_after_middle_char'
+];
+
+const OPERATORS = [
+    'equal',
+    'strong_equal',
+    'plus',
+    'minus',
+    'gt',
+    'lt',
+    'gte',
+    'lte',
+    'divide',
+    'multiply'
+];
+
+const APLHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+function getTraining() {
+    return `${getRandomValue()} ${getRandomOperator()} ${getRandomValue()}`;
+}
+
+function getRandomOperator() {
+    const operator = getRandomArrayElement(OPERATORS);
+
+    switch (operator) {
+        case 'equal':
+            return '==';
+        case 'strong_equal':
+            return '===';
+        case 'plus':
+            return '+';
+        case 'minus':
+            return '-';
+        case 'gt':
+            return '>';
+        case 'lt':
+            return '<';
+        case 'gte':
+            return '>=';
+        case 'lte':
+            return '<=';
+        case 'divide':
+            return '/';
+        case 'multiply':
+            return '*';
+        default:
+            return '==';
+    }
+}
+
+function getRandomValue() {
+    const valueType = getRandomArrayElement(VALUE_TYPES);
+    let value;
+
+    switch (valueType) {
+        case 'number':
+            value = getRandomNumber(0, 10);
+            break;
+        case 'number_as_string':
+            value = `'${(getRandomNumber(0, 10)).toString()}'`;
+            break;
+        case 'string_like_number':
+            value = `'${getStringLikeNumber()}'`;
+            break;
+        case 'array_with_number':
+            value = `'${[getRandomNumber(0, 10)]}'`;
+            break;
+        case 'empty_object':
+            value = '{}';
+            break;
+        case 'string': {
+            const aplhabet = APLHABET.split('');
+            const charsCount = getRandomNumber(1, 10);
+
+            value = `'${' '.repeat(charsCount).split('').map(() => aplhabet[getRandomNumber(0, 25)]).join('')}'`;
+            break;
+        }
+        case 'empty_string':
+            value = "' '";
+            break;
+        case 'empty_array': 
+            value = '[]';
+            break;
+        case 'nil':
+            value = 'null';
+            break;
+        case 'nan':
+            value = 'NaN'; 
+            break;         
+        default:
+            value = "' '";
+    }
+
+    const modificator = getRandomModificator();
+
+    return modificator + value;
+}
+
+function getRandomModificator() {
+    const modificator = getRandomArrayElement(MODIFICATORS);
+
+    switch(modificator) {
+        case 'none': 
+            return '';
+        case '+VAR':
+            return '+';   
+        case '-VAR':
+            return '-';
+        case '!VAR':
+            return '!';
+        case '+!VAR':
+            return '+!';
+        case '-!!VAR':
+            return '-!!';
+        case '+!!VAR':
+            return '+!!';
+        default:
+            return '';
+    }
+}
+
+function getStringLikeNumber() {
+    const aplhabet = APLHABET.split('');
+    const stringType = getRandomArrayElement(STRING_AS_NUMBER_TYPES);
+    const spacesCount = getRandomNumber(0, 3);
+    const numberFirst = getRandomNumber(0, 99);
+    const numberSecond = getRandomNumber(0, 99);
+    const chars = aplhabet[getRandomNumber(0, 25)];
+    const spaces = ' '.repeat(spacesCount); 
+    
+    switch (stringType) {
+        case 'space_before':
+            return spaces + numberFirst;
+        case 'space_before_end_char':
+            return spaces + numberFirst + chars;
+        case 'end_char': 
+            return numberFirst + chars;
+        case 'start_char':
+            return chars + numberFirst;
+        case 'start_char_space_after':
+            return chars + numberFirst + spaces;
+        case 'middle_char':
+            return numberFirst + chars + numberSecond;
+        case 'space_before_middle_char':
+            return spaces + numberFirst + chars + numberSecond;
+        case 'space_after_middle_char':
+            return numberFirst + chars + numberSecond + spaces;
+    }   
+}
+
+function getRandomArrayElement(arr) {
+    const size = arr.length;
+    const index = getRandomNumber(0, size);
+
+    return arr[index];
+}
+
+function getRandomNumber(from, to) {
+    return Math.floor(Math.random() * (to - from)) + from;
+}
+
+const result = getTraining();
+console.log(result);
